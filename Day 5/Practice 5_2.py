def4d0a8515655c39911896dbaf7a030bd89adfd
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-08-31 16:57:17 +0900

    Update Practice 5_2.py

--- Day 5/Practice 5_2.py
+++ Day 5/Practice 5_2.py
@@ -19,3 +19,8 @@ file = str_sp[3].split("=")       # FILE의 값 추출
 pz_sp = pz[3: ].split(",")       # PZ의 값을 x, y, w, h 구분하여 추출
 
 print(f"{file[0]} = {file[1]}\n{ps[0]} = {ps[1]}\n{pz[ :2]}.X = {pz_sp[0]}\n{pz[ :2]}.Y = {pz_sp[1]}\n{pz[ :2]}.W = {pz_sp[2]}\n{pz[ :2]}.H = {pz_sp[3]}")
+
+
+# "\r" CR(커서를 맨 앞으로 되돌리기) / "\n" LF(종이를 한 칸 올리기)
+# c:\user\temp\test.py => \u로 인해 오류 / \t는 탭으로 인식
+# \ -> / 로 해주거나 \\ 사용하기
