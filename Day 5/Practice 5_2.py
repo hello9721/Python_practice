365564f195837f255b99c043ba37d0af2db8143f
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-08-31 17:14:58 +0900

    Update Practice 5_2.py

--- Day 5/Practice 5_2.py
+++ Day 5/Practice 5_2.py
@@ -3,13 +3,13 @@
 # 입력된 문자열
 str = "/PS:121 /PZ:2,3,100,100 /FILE=test.py"
 
-str_sp = str.split("/")            # 옵션별로 구분
+str_sp = str.split("/")           # 옵션별로 구분
 
 ps = str_sp[1].split(":")         # PS의 값 추출
-pz = str_sp[2]                  # PZ의 값 추출
+pz = str_sp[2]                    # PZ의 값 추출
 file = str_sp[3].split("=")       # FILE의 값 추출
 
-pz_sp = pz[3: ].split(",")       # PZ의 값을 x, y, w, h 구분하여 추출
+pz_sp = pz[3: ].split(",")        # PZ의 값을 x, y, w, h 구분하여 추출
 
 print(f"{file[0]} = {file[1]}\n{ps[0]} = {ps[1]}\n{pz[ :2]}.X = {pz_sp[0]}\n{pz[ :2]}.Y = {pz_sp[1]}\n{pz[ :2]}.W = {pz_sp[2]}\n{pz[ :2]}.H = {pz_sp[3]}")
 
