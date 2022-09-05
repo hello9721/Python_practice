24d2e626dab6d59223b7ffe645614014628fb005
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-09-05 12:21:30 +0900

    Update Practice 5_3.py

--- Day 5/Practice 5_3.py
+++ Day 5/Practice 5_3.py
@@ -37,17 +37,22 @@ s = "FILE = test.py"
 print("FILE =", get_FILE(s))
 
 # str = "/PS:121 /PZ:2,3,100,100 /FILE=test.py"
+# str1 = "/PZ:2,8,109,110 /PS:150 /FILE=test1.py"
+# str2 = "/FILE=test2.py /PZ:99,8,199,110 /PS:800"
 
 # 메인 루틴
 
 def Main(s):
-    s_li = s.split("/")
-    ps = get_PS(s_li[1])
-    pz = get_PZ(s_li[2])
-    file  = get_FILE(s_li[3])
+    ps = get_PS(s[s.find("PS"): ].split("/")[0])
+    pz = get_PZ(s[s.find("PZ"): ].split("/")[0])
+    file  = get_FILE(s[s.find("FILE"): ].split("/")[0])
     
     print(f"PS = {ps} | PZ = [ x = {pz[0]}, y = {pz[1]}, w = {pz[2]}, h = {pz[3]} ] | FILE = {file}")
 
 str = "/PS:121 /PZ:2,3,100,100 /FILE=test.py"
+str1 = "/PZ:2,8,109,110 /PS:150 /FILE=test1.py"
+str2 = "/FILE=test2.py /PZ:99,8,199,110 /PS:800"
 
 Main(str)
+Main(str1)
+Main(str2)
