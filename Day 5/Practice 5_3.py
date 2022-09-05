7e3b156a8dce310b6ddfa36df18a72d6ebc009bd
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-09-05 14:10:14 +0900

    Update Practice 5_3.py

--- Day 5/Practice 5_3.py
+++ Day 5/Practice 5_3.py
@@ -17,13 +17,9 @@ def get_FILE(s):
     x = s.split("=")
     return x[1]
 
-# str = "/PS:121 /PZ:2,3,100,100 /FILE=test.py"
-# str1 = "/PZ:2,8,109,110 /PS:150 /FILE=test1.py"
-# str2 = "/FILE=test2.py /PZ:99,8,199,110 /PS:800"
+# 메인 루틴 1
 
-# 메인 루틴
-
-def Main(s):
+def Main1(s):
     try:
         ps = get_PS(s[s.find("PS"): ].split("/")[0])
         pz = get_PZ(s[s.find("PZ"): ].split("/")[0])
@@ -33,11 +29,36 @@ def Main(s):
 
     except IndexError:
         print("PS | PZ [ x, y, w, h ] | FILE 중 존재하지 않는 요소가 있어 표시 할 수 없습니다.")
+
+# 메인 루틴 2
+
+def Main2(s):
+    try:
+        s_sp = s.split("/")
         
+        for i in s_sp:
+            if i[ :2] == "PS":
+                ps = get_PS(i)
+            elif i[ :2] == "PZ":
+                pz = get_PZ(i)
+            elif i[ :2] == "FI":
+                file = get_FILE(i)
+                
+        print(f"PS = {ps} | PZ = [ x = {pz[0]}, y = {pz[1]}, w = {pz[2]}, h = {pz[3]} ] | FILE = {file}")
+
+    except IndexError:
+        print("PS | PZ [ x, y, w, h ] | FILE 중 존재하지 않는 요소가 있어 표시 할 수 없습니다.")
+
+# 결과 출력
+
 str = "/PS:121 /PZ:2,3,100,100 /FILE=test.py"
 str1 = "/PZ:2,8,109,110 /PS:150 /FILE=test1.py"
 str2 = "/FILE=test2.py /PZ:99,80,999,110 /PS:800"
 
-Main(str)
-Main(str1)
-Main(str2)
+Main1(str)
+Main1(str1)
+Main1(str2)
+
+Main2(str)
+Main2(str1)
+Main2(str2)
