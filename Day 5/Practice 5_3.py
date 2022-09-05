1f79e9d463aadc932ea4896330c9e10450a517a1
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-09-05 12:08:38 +0900

    Update Practice 5_3.py

--- Day 5/Practice 5_3.py
+++ Day 5/Practice 5_3.py
@@ -1,4 +1,16 @@
-# s = "/PZ:2,3,100,100"
+# s = "PS:121"
+
+# PS의 값만을 반환하는 함수 
+
+def get_PS(s):
+    x = s.split(":")
+    return x[1]
+
+s = "PS:121"
+
+print("PS =", get_PS(s))
+
+# s = "PZ:2,3,100,100"
 
 # PZ의 값만을 반환하는 함수 
 
@@ -23,3 +35,19 @@ def get_FILE(s):
 s = "FILE = test.py"
 
 print("FILE =", get_FILE(s))
+
+# str = "/PS:121 /PZ:2,3,100,100 /FILE=test.py"
+
+# 메인 루틴
+
+def Main(s):
+    s_li = s.split("/")
+    ps = get_PS(s_li[1])
+    pz = get_PZ(s_li[2])
+    file  = get_FILE(s_li[3])
+    
+    print(f"PS = {ps} / PZ [ x = {pz[0]}, y = {pz[1]}, w = {pz[2]}, h = {pz[3]} ] / FILE = {file}")
+
+str = "/PS:121 /PZ:2,3,100,100 /FILE=test.py"
+
+Main(str)
