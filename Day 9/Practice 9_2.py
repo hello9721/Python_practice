03f7e1cf93a9fc8d064dd172cfdedbfacdc3c4c0
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-09-14 16:59:18 +0900

    Add files via upload

---
+++ Day 9/Practice 9_2.py
@@ -0,0 +1,28 @@
+    # 문자열을 라인별로 받아서 16진수로 변환
+    # 문자열 -> 10진수   # ord(c)
+    # 10진수 -> 16진수   # {'%nx'%c} # n = 00 ~ 0F # n = 02/2 => 0a/a
+
+file = open("test3.txt")
+
+l = 1
+
+for i in file:
+    print(f" {'%3d'%l}\t{i}")
+    print(f"\n   To hex {'-'*76}  >\n\n   ", end = "")
+
+    count = 0
+    
+    for j in i:
+        c = ord(j)
+        
+        print(f"{'%02x'%c} ", end = "")
+
+        count += 1
+
+        if count%29 == 0:
+            print("\n   ", end = "")
+            
+        
+    print("\n\n")
+    
+    l += 1
