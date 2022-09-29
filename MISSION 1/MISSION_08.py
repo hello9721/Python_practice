fb2e827fd9b2d1eb19a4316cad2e2734d9cde6fa
Hana <95046369+XIUMAMU@users.noreply.github.com>
2022-09-29 16:21:20 +0900

    Update MISSION_08.py

--- MISSION 1/MISSION_08.py
+++ MISSION 1/MISSION_08.py
@@ -59,22 +59,41 @@ for i in header:
 
 cnt = []
 
-for i in range(len(name)):      # 데이터 수 만큼 반복
+# 석차를 기준으로 입력
 
-    f.write("\n")               # 줄바꿈 해주고
+for i in m_num:                 # 1등부터 꼴등까지 순서대로 정렬된 m_num에서 하나씩 꺼내와서
 
-    mean = 0                    # 사용할 평균과 석차 변수를 미리 선언
-    n = 0
-    
-    for j in m_num:             # 1등부터 꼴등까지 순서대로 정렬된 m_num에서
-                                # 하나씩 꺼내와서
-        if i == j[1]:           # i와 INDEX가 같을 때
-            mean = j[0]         # 평균은 해당 j의 평균 값이고
-            n = m_num.index(j)  # 석차는 해당 j의 인덱스 값 + 1
-                                # 학생을 INDEX로 구분하기에 동명이인이 있어도 중복 안됨.
+    f.write("\n")               # 줄바꿈
 
-    f.write(f"{name[i]},{k[i]},{m[i]},{e[i]},{c[i]},{total[i]},{mean},{n + 1}")
-                                # 데이터를 원하는 순서대로 ,로 구분하여 입력
-f.close()                       # 모두 입력이 되었으면 파일 닫기
+    mean = i[0]                 # 평균은 i의 0번째
+    n = m_num.index(i)          # 석차는 i의 인덱스
+
+    for j in range(len(name)):  # name의 수만큼 반복을 하다가
+
+        if i[1] == j:           # j가 i의 1번째와 같다면
+
+            f.write(f"{name[j]},{k[j]},{m[j]},{e[j]},{c[j]},{total[j]},{mean},{n + 1}")
+                                # 데이터 입력
+f.close()                       # 전부 입력 후 파일 닫
+
+# 원본데이터를 기준으로 입력
+
+##for i in range(len(name)):      # 데이터 수 만큼 반복
+##
+##    f.write("\n")               # 줄바꿈 해주고
+##
+##    mean = 0                    # 사용할 평균과 석차 변수를 미리 선언
+##    n = 0
+##    
+##    for j in m_num:             # 1등부터 꼴등까지 순서대로 정렬된 m_num에서
+##                                # 하나씩 꺼내와서
+##        if i == j[1]:           # i와 INDEX가 같을 때
+##            mean = j[0]         # 평균은 해당 j의 평균 값이고
+##            n = m_num.index(j)  # 석차는 해당 j의 인덱스 값 + 1
+##                                # 학생을 INDEX로 구분하기에 동명이인이 있어도 중복 안됨.
+##
+##    f.write(f"{name[i]},{k[i]},{m[i]},{e[i]},{c[i]},{total[i]},{mean},{n + 1}")
+##                                # 데이터를 원하는 순서대로 ,로 구분하여 입력
+##f.close()                       # 모두 입력이 되었으면 파일 닫기
 
 print("완료되었습니다.")
