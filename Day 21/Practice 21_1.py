945ca2ca2f642f4359e6f3e8c3914c2a7ccbfe92
Hana <hanya97@kakao.com>
2022-10-24 14:19:17 +0900

    Day 21

---
+++ Day 21/Practice 21_1.py
@@ -0,0 +1,12 @@
+# SQLite : 소형 DB / File DB => MS SQL과 비슷
+# http://sqlitebrowser.org/
+# 관계형 데이터베이스 : 테이블의 행과 열을 이용하여 데이터를 모델링
+
+# 필드 : 테이블의 열에 해당하는 부분
+# 레코드 : 테이블의 행에 해당하는 부분
+
+# select / insert / delete / update
+
+# SQL => 대소문자 구분 X
+
+# date() -> 오늘 날짜 삽입
