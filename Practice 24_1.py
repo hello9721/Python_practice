16c8d12e42e2dea39f55aab66b04ad45e11bf283
Hana <95046369+hello9721@users.noreply.github.com>
2022-11-15 16:37:57 +0900

    Add files via upload

---
+++ Practice 24_1.py
@@ -0,0 +1,11 @@
+# python -m venv {name}
+# 가상환경 생성
+
+# .\{name}\scripts\activate
+# 가상환경 실행
+
+# pip install django~=2.0.0
+# Django 설치
+
+# django-admin.py startproject {projectName} {projectDirectory}
+# 프로젝트 생성
