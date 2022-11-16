bd115f4560f927a7487f90ae2e8ffca5bb246a18
Hana <95046369+hello9721@users.noreply.github.com>
2022-11-16 17:23:38 +0900

    Update Practice 25_1.py

--- Day 25/Practice 25_1.py
+++ Day 25/Practice 25_1.py
@@ -25,3 +25,10 @@
 
 # 127.0.0.1:8000/admin/ 으로 접속 후 설정한 이름과 비밀번호로 접근
 # Django 관리 창이 뜬다.
+
+# models.py 에 모델 클래스 작성
+# publish는 꼭 define 해주기
+
+# manage.py makemigrations {name}
+# manage.py migrate {name}
+# 모델 적용
