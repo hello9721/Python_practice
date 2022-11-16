6d0ad30202d877a561ee567b7e04cf1513fe884a
Hana <95046369+hello9721@users.noreply.github.com>
2022-11-16 15:25:50 +0900

    Add files via upload

---
+++ Practice 25_1.py
@@ -0,0 +1,27 @@
+# md {folderName}
+# 새폴더 생성
+
+# rd {folderName}
+# 비어있는 폴더 삭제
+
+# rd -s {folderName}
+# 비어있지 않아도 삭제
+
+# del {folderName}
+# 비어있지 않아도 삭제
+
+# 프로젝트 폴더에서 >
+
+# manage.py migrate
+# manage.py startapp {name}
+
+# 앱 추가
+
+# manage.py createsuperuser
+# {userName}      # me
+# {emailAddress}  # hello9721@naver.com
+# {password}      # kiki9908
+# {password}      # kiki9908
+
+# 127.0.0.1:8000/admin/ 으로 접속 후 설정한 이름과 비밀번호로 접근
+# Django 관리 창이 뜬다.
