a76ec785f6d530b4f37278ec54bafcc6264c9360
Hana <hanya97@kakao.com>
2022-12-14 11:40:25 +0900

    db connect

--- MINI PROJECT/Project_Weather/Website/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import *
-
-def home(req):
-
-    lst = Srtncst.objects.all()
-    context = {'list' : lst}
-
-    return render(req, 'home/index.html', context)
